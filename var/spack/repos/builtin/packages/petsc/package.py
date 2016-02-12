9e29ab587a95c573293b1b5909ac8067bd5d47d5
Luigi Calori <luigi.calori@gmail.com>
2016-02-12 18:07:49 +0100

    improving petsc package adding variants

--- var/spack/repos/builtin/packages/petsc/package.py
+++ var/spack/repos/builtin/packages/petsc/package.py
@@ -12,6 +12,10 @@ class Petsc(Package):
     version('3.5.2', 'ad170802b3b058b5deb9cd1f968e7e13')
     version('3.5.1', 'a557e029711ebf425544e117ffa44d8f')
 
+    variant('shared', default=False, description='Build shared')
+    variant('debug', default=True, description='Build with debugging')
+
+
     depends_on("python @2.6:2.9")   # requires Python for building
 
     depends_on("boost")
@@ -24,6 +28,10 @@ class Petsc(Package):
     depends_on("mpi")
 
     def install(self, spec, prefix):
+        def feature_to_bool(feature, on=1, off=0):
+            if feature in spec:
+                return on
+            return off
         configure("--prefix=%s" % prefix,
                   "--with-blas-lib=%s/libblas.a"     % spec['blas'].prefix.lib,
                   "--with-lapack-lib=%s/liblapack.a" % spec['lapack'].prefix.lib,
@@ -33,7 +41,8 @@ class Petsc(Package):
                   "--with-metis-dir=%s"              % spec['metis'].prefix,
                   "--with-hdf5-dir=%s"               % spec['hdf5'].prefix,
                   "--with-mpi-dir=%s"                % spec['mpi'].prefix,
-                  "--with-shared-libraries=0")
+                  "--with-shared-libraries=%s" % feature_to_bool('+shared'),
+                  "--with-debugging=%s" % feature_to_bool('+debug'))
 
         # PETSc has its own way of doing parallel make.
         make('MAKE_NP=%s' % make_jobs, parallel=False)
