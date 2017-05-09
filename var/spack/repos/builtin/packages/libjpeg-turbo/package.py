1e71a2c878c984aab6a66306c0b5580948adbb2b
Luigi Calori <luigi.calori@gmail.com>
2017-05-09 04:13:53 +0200

    refactor turbovnc, added version, first step in build java, flake8

--- var/spack/repos/builtin/packages/libjpeg-turbo/package.py
+++ var/spack/repos/builtin/packages/libjpeg-turbo/package.py
@@ -23,6 +23,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import os
 
 
 class LibjpegTurbo(Package):
@@ -44,7 +45,6 @@ class LibjpegTurbo(Package):
     provides('jpeg')
     variant('java', default=False, description='Enable Java build')
 
-
     # Can use either of these. But in the current version of the package
     # only nasm is used. In order to use yasm an environmental variable
     # NASM must be set.
@@ -55,6 +55,7 @@ class LibjpegTurbo(Package):
     depends_on('automake', type='build', when="@1.3.1:1.5.3")
     depends_on('libtool', type='build', when="@1.3.1:1.5.3")
     depends_on('cmake', type='build', when="@1.5.90:")
+    depends_on('jdk', when='+java', type='build')
 
     @property
     def libs(self):
@@ -75,11 +76,17 @@ class LibjpegTurbo(Package):
             cmake('..', *cmake_args)
             make()
             make('install')
-    depends_on('jdk', when='+java')
-    
+
     def configure_args(self):
         args = []
         if '+java' in self.spec:
             args.append('--with-java')
+            # args.append('--with-java=' + self.spec['jdk'].prefix)
         return args
-    
+
+    def setup_environment(self, spack_env, run_env):
+        spack_env.set(
+            'JNI_CFLAGS',
+            '-I' + os.path.join(self.spec['jdk'].prefix, 'include') + ' ' +
+            '-I' + os.path.join(self.spec['jdk'].prefix, 'include', 'linux'),
+            separator=' ')
