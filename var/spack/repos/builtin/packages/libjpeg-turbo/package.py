71c30b1cb4e6c3aaefd3b5baf0a01659866b5a4d
Luigi Calori <luigicalori@dhcp56-v72.pdl.cineca.it>
2017-12-21 00:53:24 +0100

    fix build under OSX, including platform dependent jni folder

--- var/spack/repos/builtin/packages/libjpeg-turbo/package.py
+++ var/spack/repos/builtin/packages/libjpeg-turbo/package.py
@@ -23,6 +23,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
+import os
+import sys
 
 
 class LibjpegTurbo(AutotoolsPackage):
@@ -65,5 +67,5 @@ class LibjpegTurbo(AutotoolsPackage):
                 'JNI_CFLAGS',
                 '-I' + self.spec['jdk'].prefix.include +
                 ' ' + '-I' +
-                self.spec['jdk'].prefix.include.linux,
+                os.path.join(self.spec['jdk'].prefix.include, sys.platform),
                 separator=' ')
