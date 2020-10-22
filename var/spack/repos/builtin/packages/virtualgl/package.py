5274325d674667c0f129f38ca66657fd54155050
Luigi Calori <l.calori@cineca.it>
2020-10-22 16:38:08 +0200

    upgrade virtualgl to 2.6.4, add needed X11 dependencies, disable opencl, TODO: apply only for needed versions

--- var/spack/repos/builtin/packages/virtualgl/package.py
+++ var/spack/repos/builtin/packages/virtualgl/package.py
@@ -16,7 +16,38 @@ class Virtualgl(CMakePackage):
     homepage = "http://www.virtualgl.org/Main/HomePage"
     url      = "http://downloads.sourceforge.net/project/virtualgl/2.5.2/VirtualGL-2.5.2.tar.gz"
 
+    version('2.6.4', sha256='39978f737ffe73e426e9a921486b9f6cee9c9b9ab9e82d2ae4e2ffe3b27398e5')
     version('2.5.2', sha256='4f43387678b289a24139c5b7c3699740ca555a9f10011c979e51aa4df2b93238')
 
     depends_on("jpeg")
     depends_on("glu")
+    depends_on("libxcb")
+    depends_on("xcb-util-keysyms")
+    depends_on("libx11")
+    depends_on("libxau")
+    depends_on("libxrender")
+    depends_on("libxrandr")
+    depends_on("libxinerama")
+    depends_on("libxft")
+    depends_on("libxmu")
+    depends_on("libxpm")
+    depends_on("libxscrnsaver")
+    depends_on("libxv")
+    depends_on("libxt")
+    depends_on("libxres")
+    depends_on("libxcomposite")
+    depends_on("libxcursor")
+    depends_on("libxdamage")
+    depends_on("libxtst")
+    depends_on("libxkbfile")
+    depends_on("libxxf86vm")
+    depends_on("libxxf86misc")
+
+    def cmake_args(self):
+        spec = self.spec
+
+        cmake_args = [
+            '-DVGL_FAKEOPENCL=OFF'
+        ]
+        return cmake_args
+
